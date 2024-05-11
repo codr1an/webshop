c1475d9bf796372538abe393b240b461e2fbca67
codr1an <adi_popa2002@yahoo.com>
2024-05-11 13:40:15 +0200

    Add category pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,12 @@ import LoginForm from "./Components/LoginForm/LoginForm";
 import RegisterForm from "./Components/RegisterForm/RegisterForm";
 import Home from "./Components/Home/Home";
 import Test from "./Components/test";
+import LaptopPage from "./Components/Categories/Laptop"
+import PhonePage from "./Components/Categories/Phone"
+import TabletPage from "./Components/Categories/Tablet"
+import TvPage from "./Components/Categories/Tv"
+import MonitorPage from "./Components/Categories/Monitor"
+
 
 const App = () => {
   return (
@@ -14,6 +20,12 @@ const App = () => {
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/home" element={<Home />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/laptops" element={<LaptopPage />} />
+        <Route path="/monitors" element={<MonitorPage />} />
+        <Route path="/phones" element={<PhonePage />} />
+        <Route path="/tvs" element={<TvPage />} />
+        <Route path="/tablets" element={<TabletPage />} />
+
       </Routes>
     </Router>
   );
